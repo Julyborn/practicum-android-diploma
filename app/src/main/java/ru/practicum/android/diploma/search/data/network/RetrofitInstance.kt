1d7ca785368fab20a81281a1cbbad92673551c2d
Raigou <paladi.post@gmail.com>
2024-10-05 14:21:06 +0300

    add AuthInterceptor

--- app/src/main/java/ru/practicum/android/diploma/search/data/network/RetrofitInstance.kt
+++ app/src/main/java/ru/practicum/android/diploma/search/data/network/RetrofitInstance.kt
@@ -1,18 +1,27 @@
 package ru.practicum.android.diploma.search.data.network
 
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-private const val HH_URL = "https://api.hh.ru/"
-
 object RetrofitInstance {
+
+    private const val HH_URL = "https://api.hh.ru/"
+    private const val AUTH_TOKEN = "access_token"
+    private const val USER_AGENT = "user_agent"
+
+    private val okHttpClient: OkHttpClient by lazy {
+        OkHttpClient.Builder()
+            .addInterceptor(AuthInterceptor(AUTH_TOKEN, USER_AGENT))
+            .build()
+    }
 
     val headHunterAPI: HeadHunterAPI by lazy {
         Retrofit.Builder()
             .baseUrl(HH_URL)
+            .client(okHttpClient)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
             .create(HeadHunterAPI::class.java)
     }
-
 }
