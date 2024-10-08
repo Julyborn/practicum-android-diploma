c5908bb6c4e51961af4a649ff1a86c549f964c33
Мyachkaa <myachkaaa@ya.ru>
2024-10-08 18:36:48 +0300

    Верстка экрана "Выбор места работы"
    Также, добавлены фрагменты для экранов "Выбор страны" и "Выбор региона" и сверстаны экраны.

---
+++ app/src/main/java/ru/practicum/android/diploma/filter/ui/CountryChoosingFragment.kt
@@ -0,0 +1,23 @@
+package ru.practicum.android.diploma.filter.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import ru.practicum.android.diploma.R
+
+class CountryChoosingFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_region_choosing, container, false)
+    }
+}
