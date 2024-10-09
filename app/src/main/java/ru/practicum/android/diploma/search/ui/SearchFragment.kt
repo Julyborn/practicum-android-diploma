34da08b77da9686e628841395d951f0bf376592c
peter <peter.hero2000@yandex.ru>
2024-10-09 22:51:49 +0300

    Убрал варнинги

--- app/src/main/java/ru/practicum/android/diploma/search/ui/SearchFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/search/ui/SearchFragment.kt
@@ -100,12 +100,6 @@ class SearchFragment : Fragment() {
         }
 
         binding.searchEditText.addTextChangedListener(object : TextWatcher {
-            override fun afterTextChanged(s: Editable?) {
-            }
-
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-            }
-
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
                 if (s.isNullOrEmpty()) {
                     binding.searchIcon.visibility = View.VISIBLE
