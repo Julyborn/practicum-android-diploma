5a8200e6d62ae55ab1bb973fd74cc39a40e56907
Raigou <paladi.post@gmail.com>
2024-10-10 00:00:49 +0300

    edit VacancySearchParams

--- app/src/main/java/ru/practicum/android/diploma/search/domain/models/VacancySearchParams.kt
+++ app/src/main/java/ru/practicum/android/diploma/search/domain/models/VacancySearchParams.kt
@@ -2,14 +2,25 @@ package ru.practicum.android.diploma.search.domain.models
 
 data class VacancySearchParams(
     val query: String = "",
+    val location: String? = null,
+    val industry: String? = null,
+    val salary: Int? = null,
+    val hideWithoutSalary: Boolean = false,
     val perPage: Int = 20,
     val page: Int = 0,
 )
 
 fun VacancySearchParams.toMap(): Map<String, String> {
-    return mapOf(
-        "text" to query,
-        "per_page" to perPage.toString(),
-        "page" to page.toString()
+    val params = mutableMapOf(
+        "text" to query, "per_page" to perPage.toString(), "page" to page.toString()
     )
+
+    location?.let { params["location"] = it }
+    industry?.let { params["industry"] = it }
+    salary?.let { params["salary"] = it.toString() }
+    if (hideWithoutSalary) {
+        params["only_with_salary"] = "true"
+    }
+
+    return params
 }
