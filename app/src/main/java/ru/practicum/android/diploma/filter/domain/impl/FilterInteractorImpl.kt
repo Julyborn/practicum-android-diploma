64378f84884e3f0285f039cbc6a7cfdc92c54367
Raigou <paladi.post@gmail.com>
2024-10-15 03:35:26 +0300

    edit FilterInteractorImpl

--- app/src/main/java/ru/practicum/android/diploma/filter/domain/impl/FilterInteractorImpl.kt
+++ app/src/main/java/ru/practicum/android/diploma/filter/domain/impl/FilterInteractorImpl.kt
@@ -1,6 +1,15 @@
 package ru.practicum.android.diploma.filter.domain.impl
 
+import ru.practicum.android.diploma.filter.data.dto.FilterSettings
 import ru.practicum.android.diploma.filter.domain.api.FilterInteractor
 import ru.practicum.android.diploma.filter.domain.api.FilterRepository
 
-class FilterInteractorImpl(repository: FilterRepository) : FilterInteractor
+class FilterInteractorImpl(private val filterRepository: FilterRepository) : FilterInteractor {
+    override fun saveFilterSettings(filterSettings: FilterSettings) {
+        filterRepository.saveFilterSettings(filterSettings)
+    }
+
+    override fun loadFilterSettings(): FilterSettings {
+        return filterRepository.loadFilterSettings()
+    }
+}
