51c11c95e7d76772ec97259ada4b5130c1f39f8f
Raigou <paladi.post@gmail.com>
2024-10-15 21:37:11 +0300

    edit FilterViewModel

--- app/src/main/java/ru/practicum/android/diploma/filter/presentation/FilterViewModel.kt
+++ app/src/main/java/ru/practicum/android/diploma/filter/presentation/FilterViewModel.kt
@@ -1,17 +1,23 @@
 package ru.practicum.android.diploma.filter.presentation
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import ru.practicum.android.diploma.filter.data.dto.FilterSettings
 import ru.practicum.android.diploma.filter.domain.api.FilterInteractor
 
 class FilterViewModel(private val filterInteractor: FilterInteractor) : ViewModel() {
 
+    fun loadFilters(): FilterSettings {
+        return filterInteractor.loadFilterSettings()
+    }
+
     fun applyFilters(location: String?, industry: String?, salary: String?, hideWithoutSalary: Boolean) {
         val filterSettings = FilterSettings(location, industry, salary, hideWithoutSalary)
+
         filterInteractor.saveFilterSettings(filterSettings)
     }
 
-    fun loadFilters(): FilterSettings {
-        return filterInteractor.loadFilterSettings()
+    fun clearFilters() {
+        filterInteractor.clearFilterSettings()
     }
 }
