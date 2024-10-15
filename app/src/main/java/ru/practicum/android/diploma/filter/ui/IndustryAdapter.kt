470fb8b808efa84123980eb9476bf6b62897e3e5
Raigou <paladi.post@gmail.com>
2024-10-15 03:38:23 +0300

    add IndustryAdapter

---
+++ app/src/main/java/ru/practicum/android/diploma/filter/ui/IndustryAdapter.kt
@@ -0,0 +1,67 @@
+package ru.practicum.android.diploma.filter.ui
+
+import android.annotation.SuppressLint
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.RadioButton
+import androidx.recyclerview.widget.RecyclerView
+import ru.practicum.android.diploma.R
+import ru.practicum.android.diploma.filter.data.dto.IndustryDto
+
+class IndustryAdapter(
+    private val onClick: (String, Int) -> Unit
+) : RecyclerView.Adapter<IndustryAdapter.IndustryViewHolder>() {
+
+    private var industries = listOf<IndustryDto>()
+    private var selectedPosition: Int = RecyclerView.NO_POSITION
+
+    // Обновление списка
+    fun update(newIndustries: List<IndustryDto>) {
+        industries = newIndustries
+        notifyDataSetChanged()
+    }
+
+    class IndustryViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val industryName: RadioButton = itemView.findViewById(R.id.industryName)
+
+        fun bind(industry: IndustryDto, isSelected: Boolean) {
+            industryName.text = industry.name
+            industryName.isChecked = isSelected
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): IndustryViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_industry, parent, false)
+        return IndustryViewHolder(view)
+    }
+
+    fun getSelectedIndustry(): IndustryDto? {
+        return if (selectedPosition != RecyclerView.NO_POSITION) {
+            industries[selectedPosition]
+        } else {
+            null
+        }
+    }
+
+    override fun onBindViewHolder(holder: IndustryViewHolder, @SuppressLint("RecyclerView") position: Int) {
+        val industry = industries[position]
+        val isSelected = selectedPosition == position
+        holder.bind(industry, isSelected)
+
+        holder.itemView.setOnClickListener {
+            if (selectedPosition != position) {
+                val previousPosition = selectedPosition
+                selectedPosition = position
+
+                notifyItemChanged(previousPosition)
+                notifyItemChanged(position)
+            }
+
+            onClick(industry.id, position)
+        }
+    }
+
+    override fun getItemCount(): Int = industries.size
+}
