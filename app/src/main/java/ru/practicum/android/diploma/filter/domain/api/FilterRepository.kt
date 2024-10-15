3b7b7e1619e52c20977a755d8b63880c2de957e2
Raigou <paladi.post@gmail.com>
2024-10-15 03:36:02 +0300

    edit FilterRepository

--- app/src/main/java/ru/practicum/android/diploma/filter/domain/api/FilterRepository.kt
+++ app/src/main/java/ru/practicum/android/diploma/filter/domain/api/FilterRepository.kt
@@ -1,3 +1,8 @@
 package ru.practicum.android.diploma.filter.domain.api
 
-interface FilterRepository
+import ru.practicum.android.diploma.filter.data.dto.FilterSettings
+
+interface FilterRepository {
+    fun saveFilterSettings(filterSettings: FilterSettings)
+    fun loadFilterSettings(): FilterSettings
+}
