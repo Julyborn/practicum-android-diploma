3c03253bc56ced58b0a3f2b904d908e389566f49
Raigou <paladi.post@gmail.com>
2024-10-15 03:36:14 +0300

    edit FilterRepositoryImpl

--- app/src/main/java/ru/practicum/android/diploma/filter/data/impl/FilterRepositoryImpl.kt
+++ app/src/main/java/ru/practicum/android/diploma/filter/data/impl/FilterRepositoryImpl.kt
@@ -1,5 +1,21 @@
 package ru.practicum.android.diploma.filter.data.impl
 
+import ru.practicum.android.diploma.filter.data.dto.FilterSettings
+import ru.practicum.android.diploma.filter.domain.api.FilterPreferences
 import ru.practicum.android.diploma.filter.domain.api.FilterRepository
 
-class FilterRepositoryImpl : FilterRepository
+class FilterRepositoryImpl(private val filterPreferences: FilterPreferences) : FilterRepository {
+
+    override fun saveFilterSettings(filterSettings: FilterSettings) {
+        filterPreferences.saveFilters(
+            filterSettings.location,
+            filterSettings.industry,
+            filterSettings.salary,
+            filterSettings.hideWithoutSalary
+        )
+    }
+
+    override fun loadFilterSettings(): FilterSettings {
+        return filterPreferences.getFilters()
+    }
+}
