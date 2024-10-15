7a5b6bf05d924d0ffcc5699e9e0bdad55002bae6
Raigou <paladi.post@gmail.com>
2024-10-15 03:35:08 +0300

    edit FilterInteractor

--- app/src/main/java/ru/practicum/android/diploma/filter/domain/api/FilterInteractor.kt
+++ app/src/main/java/ru/practicum/android/diploma/filter/domain/api/FilterInteractor.kt
@@ -1,3 +1,8 @@
 package ru.practicum.android.diploma.filter.domain.api
 
-interface FilterInteractor
+import ru.practicum.android.diploma.filter.data.dto.FilterSettings
+
+interface FilterInteractor {
+    fun saveFilterSettings(filterSettings: FilterSettings)
+    fun loadFilterSettings(): FilterSettings
+}
