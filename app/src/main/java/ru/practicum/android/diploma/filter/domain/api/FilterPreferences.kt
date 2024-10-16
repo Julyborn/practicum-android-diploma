e3e72c741a076e3b2c424df7a8b79e08d3994ef8
Raigou <paladi.post@gmail.com>
2024-10-16 04:33:38 +0300

    edit FilterPreferences

--- app/src/main/java/ru/practicum/android/diploma/filter/domain/api/FilterPreferences.kt
+++ app/src/main/java/ru/practicum/android/diploma/filter/domain/api/FilterPreferences.kt
@@ -3,7 +3,7 @@ package ru.practicum.android.diploma.filter.domain.api
 import ru.practicum.android.diploma.filter.data.dto.FilterSettings
 
 interface FilterPreferences {
-    fun saveFilters(location: String?, industry: String?, salary: String?, hideWithoutSalary: Boolean)
+    fun saveFilters(location: String?, industry: String?, salary: String?, industryId: String?, hideWithoutSalary: Boolean)
     fun getFilters(): FilterSettings
     fun clearFilters()
 }
