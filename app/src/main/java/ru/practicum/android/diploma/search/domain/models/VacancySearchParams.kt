a0f856a861d3b22368aaab62a7991a4b8adca9f7
Raigou <paladi.post@gmail.com>
2024-10-16 04:39:06 +0300

    edit VacancySearchParams

--- app/src/main/java/ru/practicum/android/diploma/search/domain/models/VacancySearchParams.kt
+++ app/src/main/java/ru/practicum/android/diploma/search/domain/models/VacancySearchParams.kt
@@ -1,9 +1,13 @@
 package ru.practicum.android.diploma.search.domain.models
 
+import android.util.Log
+
 data class VacancySearchParams(
     val query: String = "",
     val location: String? = null,
-    val industry: String? = null,
+    val selectedCountry: String? = null,
+    val selectedRegion: String? = null,
+    val industryId: String? = null,
     val salary: Int? = null,
     val hideWithoutSalary: Boolean = false,
     val perPage: Int = 20,
@@ -16,8 +20,13 @@ fun VacancySearchParams.toMap(): Map<String, String> {
     )
 
     location?.let { params["location"] = it }
-    industry?.let { params["industry"] = it }
+
+    selectedCountry?.let { params["country"] = it }
+    selectedRegion?.let { params["region"] = it }
+
+    industryId?.let { params["industry"] = it }
     salary?.let { params["salary"] = it.toString() }
+
     if (hideWithoutSalary) {
         params["only_with_salary"] = "true"
     }
