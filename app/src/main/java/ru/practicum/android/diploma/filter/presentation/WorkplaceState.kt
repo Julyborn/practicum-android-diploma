e1850c551e661a9555f9de8567b0018201ebb0f9
Raigou <paladi.post@gmail.com>
2024-10-24 12:04:09 +0300

    edit WorkplaceState

--- app/src/main/java/ru/practicum/android/diploma/filter/presentation/WorkplaceState.kt
+++ app/src/main/java/ru/practicum/android/diploma/filter/presentation/WorkplaceState.kt
@@ -2,10 +2,12 @@ package ru.practicum.android.diploma.filter.presentation
 
 import ru.practicum.android.diploma.filter.domain.models.Country
 import ru.practicum.android.diploma.filter.domain.models.Region
+import ru.practicum.android.diploma.search.presentation.models.UiScreenState
 
 sealed class WorkplaceState {
     data object Loading : WorkplaceState()
     data class Success(val countries: List<Country>, val regions: List<Region>?) : WorkplaceState()
     data object NoRegionsError : WorkplaceState()
     data object FetchError : WorkplaceState()
+    data object NoInternet : WorkplaceState()
 }
